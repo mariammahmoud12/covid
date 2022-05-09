acceb4b8f4bd74309f846956f6378b30c1cc2eee
Mustafa Mahmoud <moustafa.sasa14.mm1@gmail.com>
2022-05-09 22:37:30 +0200

    my commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: SplashScreen(),
+      home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
